6f06eb459e7a0fbee703aec5c65bd443154621df
Dunaevd <dunaevskiy@bk.ru>
2020-04-11 19:45:31 +0300

    add lesson 06

--- mine.js
+++ mine.js
@@ -1,24 +1,37 @@
 "use strict";
 
-let learnNumber = () => {
+let guessNumber = () => {
+     
+    let xNumber = Math.floor(Math.random() * 100 + 1); //Рондомное число
+    
+    let userNumber = +prompt('Угадай число от 1 до 100');//число которое вводит прользователь.
 
-    let xNumber = Math.floor(Math.random() * 100 + 1);
+    let win; //Число угадано
+    
 
-    console.log(xNumber);
+    if ((Number.isNaN(parseFloat(userNumber)))) {
+    confirm('Введите число');
+    return guessNumber();
+    }
 
-        let userNumber = prompt('Угадай число от 1 до 100');
+    if (userNumber > 0 && userNumber < xNumber) {
+        alert('Загаданное число больше');
+        return guessNumber();
+    } else if (userNumber > xNumber){
+        alert('Загаданное число меньше');
+        return guessNumber();
+    }
 
-        console.log(typeof(userNumber));
-        console.log('cruu ' + userNumber);
-        console.log('boolean ' +  Boolean(userNumber));
+    if (userNumber === xNumber){
+        win = confirm('Вы угадали число!! Хотите сыграть еще?');
+    }
 
-        if (userNumber > xNumber) {
-            confirm('Загаданное число меньше');
-            
-        } else {
-             confirm('Загаданное число больше');
-                 
-        }
-         
+    if (win === true){
+        return guessNumber();
+    } else {
+        alert('Игра закончена');
+    }
+
 };
-learnNumber();
+
+guessNumber();
